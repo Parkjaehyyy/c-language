8d91ed3882f22191ffe1551f82f0f50da90204a1
Parkjaehyyy <poiuy263@naver.com>
2024-10-29 22:14:21 +0900

    인트로 변수를 3개 선언하기 숙제

--- c-language/index.c
+++ c-language/index.c
@@ -2,13 +2,17 @@
 // 정수와 실수 
 // 정수형 : short - 2byte  / int - 4 byte  / long - 8byte
 // 실수형 : float - 4byte / double - 8byte /
+// bool = 1bit 
+// char = 2byte 
+
+//int로 변수를 3개 선언을 하고 한줄의 프린트문으로 3개의 변수 
 #include<stdio.h>
-main() {
+int main() {
 	short a = 4;
 	int b = 4;
 	long c = 8;
-	float d = 8;
-	double e = 8;
+	float d = 8.0f;
+	double e = 8.0;
 
 
 	
@@ -19,8 +23,13 @@ main() {
 	printf("%f\n", d);
 	printf("%f\n", e);
 	// 이스케이프 시컨스 
-	printf("\"이스\' 케이프 \' 시컨스\"");
+	printf("\"avc\'ave \'qwea\"");
 
+
+
+
+	return 0;
+	
 }
 
 

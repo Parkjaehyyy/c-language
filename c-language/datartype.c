2db4f0714b48147528b81793011ed9aa70f7cca3
Parkjaehyyy <poiuy263@naver.com>
2024-10-29 22:00:36 +0900

    이스케이프시컨스

---
+++ c-language/datartype.c
@@ -0,0 +1,5 @@
+//#include <stdio.h>
+//int main() { 
+//	printf("안녕하세요\n");
+//	printf("박재현입니다.\n");
+//}
--- c-language/index.c
+++ c-language/index.c
@@ -1,5 +1,32 @@
-#include <stdio.h>
-int main() { 
-	printf("안녕하세요\n");
-	printf("박재현입니다.\n");
+// 숫자
+// 정수와 실수 
+// 정수형 : short - 2byte  / int - 4 byte  / long - 8byte
+// 실수형 : float - 4byte / double - 8byte /
+#include<stdio.h>
+main() {
+	short a = 4;
+	int b = 4;
+	long c = 8;
+	float d = 8;
+	double e = 8;
+
+
+	
+	// 서식 문자 %d < 10진법 - %f < 실수  
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+	printf("%f\n", d);
+	printf("%f\n", e);
+	// 이스케이프 시컨스 
+	printf("\"이스\' 케이프 \' 시컨스\"");
+
 }
+
+
+// 문자
+
+
+
+
+// 논리

3842639a22adb53526dea62927f260dfe4ecd498
Parkjaehyyy <poiuy263@naver.com>
2024-11-12 21:18:41 +0900

    data 오타 발견

--- c-language/datartype.c
+++ c-language/datartype.c
@@ -1,5 +1,17 @@
-//#include <stdio.h>
-//int main() { 
-//	printf("안녕하세요\n");
-//	printf("박재현입니다.\n");
-//}
+#include<stdio.h>
+int main() {
+	int a = 10, b = 50, c = 70;
+	printf("%d,%d,%d\n", a, b, c);
+	return 0;
+}
+
+/*
+#include <stdio.h>
+int main() {
+	printf("안녕하세요\n");
+	printf("박재현입니다.\n");
+	return0;
+}
+*/
+
+

a9dcb899ab8d282b1637077ed0375a120767b981
Parkjaehyyy <poiuy263@naver.com>
2024-11-12 21:16:49 +0900

    숙제 제출
    sln 안으로 코드 이동

---
+++ c-language/homework.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+	int a = 10, b = 50, c = 70;
+	printf("%d,%d,%d\n", a, b, c);
+	return 0;
+}
+
+
--- homework.c
+++
@@ -1,8 +0,0 @@
- #include<stdio.h>
-int main() {
-	int a = 10, b = 50, c = 70;
-		printf("%d,%d,%d\n", a, b, c);
- return 0 ;
-}
-
-

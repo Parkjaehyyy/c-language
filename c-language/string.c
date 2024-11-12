6468cb8881892966bed5d02ec8cda7c334af9749
Parkjaehyyy <poiuy263@naver.com>
2024-11-12 21:49:50 +0900

    사칙연산 예제 추가

--- c-language/string.c
+++ c-language/string.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
 int main() {
-
+	// 사칙연산
+	int a = 20; 
+	int b = 10;
+	printf("%d + %d = %d\n", a, b, a+b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d %% %d = %d\n", a, b, a % b); // 나머지 연산
+	
+	
 	return 0;
 }
 

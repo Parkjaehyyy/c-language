724baa5c80611ddc695643732517a35fe2f66d99
Parkjaehyyy <poiuy263@naver.com>
2024-11-12 21:40:47 +0900

    정수.문자.실수 출력실시.

--- c-language/datartype.c
+++ c-language/datartype.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-int main() {
-	int a = 10, b = 50, c = 70;
-	printf("%d,%d,%d\n", a, b, c);
-	return 0;
-}
+//#include<stdio.h>
+//int main() {
+//	int a = 10, b = 50, c = 70;
+//	printf("%d,%d,%d\n", a, b, c);
+//	return 0;
+//}
 
 /*
 #include <stdio.h>
@@ -14,4 +14,20 @@ int main() {
 }
 */
 
+
+//int main() {
+//	// 실수, 정수, 문자, 논리 변수를 하나씩 선언 혹은 초기화하고
+//	// 출력해주세요
+//	int a = 10;
+//	printf("%d\n", a);
+//
+//	char b = 'a';
+//	printf("%c\n", b);
+//
+//	float c = 3.14f;
+//	printf("%f\n", c);
+//
+//
+//	return 0;
+//}
 

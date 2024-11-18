f1f099d2704f86fa0e857d2fe22c474df3d58ba7
Parkjaehyyy <poiuy263@naver.com>
2024-11-18 20:48:15 +0900

    숙제 증감연산 과제

--- c-language/homework.c
+++ c-language/homework.c
@@ -5,4 +5,19 @@
 //	return 0;
 //}
 
+#include<stdio.h>
+int main(){
+	int number = 1;
+		printf("%d\n",number++); // 1 (후위 증가 ) //printf("%d\n", number); > 2 (위에 후위 증가이기때문에 number는 2으로 출력)
+		printf("%d\n", ++number);  // 3 ( 위 문법에서 number이 2에서 시작하고 전위 ++ 이기 때문에 출력을하게되면 3으로 출력)
+		printf("%d\n", number--); // 3 ( 2번째 문법에서 3으로 시작하고 후위 감소이기 때문에 출력시 3으로 출력)
+		printf("%d\n", --number); // 1 ( 위 문법에서 후위 감소이기 때문에 2가되었고 현재 문법에서 -- 전위감소가 나오기때문에 1으로 출력)
+		printf("%d\n", ++number); // 2 (전 문법에서 1로 출력이되고 ++ 전위 증가이기 때문에 2로 출력)
+		printf("%d\n", number);  // 2 (현재 문법에서는 증가와 감소가 없기 때문에 2로 출력)
+		printf("%d\n", --number); // 1 ( 위 문법에서 -- 전위 감소이기 때문에 1로 출력)
+		printf("%d\n", ++number); // 2 ( 위 문법에서 1로 출력이 되었기문에 ++전위 증가 2로 출력 )
 
+
+
+return 0 ;
+}
--- c-language/string.c
+++ c-language/string.c
@@ -1,30 +1,31 @@
-#include<stdio.h>
+/* 
+ #include<stdio.h>
 
 int main() {
-	// 사칙연산
+	 사칙연산
 	int a = 20; 
 	int b = 10;
-	//printf("%d + %d = %d\n", a, b, a+b);
-	//printf("%d - %d = %d\n", a, b, a - b);
-	//printf("%d / %d = %d\n", a, b, a / b);
-	//printf("%d * %d = %d\n", a, b, a * b);
-	//printf("%d %% %d = %d\n", a, b, a % b); // 나머지 연산
+	printf("%d + %d = %d\n", a, b, a+b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d %% %d = %d\n", a, b, a % b); // 나머지 연산
 
 
-	// 대입연산
-	//a = a + 10;
-	//printf("%d \n", a);
-	//a += 10;
-	//printf("%d \n", a);
-	//a -= 10; // a = a - 10;
-	//printf("%d \n", a);
-	//a *= 10; // a = a * 10;
-	//printf("%d \n", a);
-	//a %= 6; // a = a % 6;
-	//printf("%d \n", a);
+	 대입연산
+	a = a + 10;
+	printf("%d \n", a);
+	a += 10;
+	printf("%d \n", a);
+	a -= 10; // a = a - 10;
+	printf("%d \n", a);
+	a *= 10; // a = a * 10;
+	printf("%d \n", a);
+	a %= 6; // a = a % 6;
+	printf("%d \n", a);
 	
 	
-	// 증감연산
+증감연산
 	a++; // a = a + 1;
 	++a; // a = a + 1;
 
@@ -32,10 +33,10 @@ int main() {
 	--a; // a = a - 1;
 
 	int number = 0;
-	//printf("%d \n", number++);
-	//printf("%d \n", number);
+	printf("%d \n", number++);
+	printf("%d \n", number);
 
-	//printf("%d \n", ++number);
+	printf("%d \n", ++number);
 
 	printf("%d \n", number++); //  0
 	printf("%d \n", --number); // 0
@@ -43,48 +44,37 @@ int main() {
 	printf("%d \n", number--); // 0
 	printf("%d \n", number); // 0
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	return 0;
 }
 
 
 
-// 숫자, 문자, 논리
-// 숫자 - 실수 정수
-// 문자 - 
-// 문자에 대해 아는만큼
-// 문자와 대입되는 숫자가 있다
-// 아스키코드 128개 정도 묶어둠 -> 부족함
-// 한글 몇개일까요?
-// 유니코드 > 변천사 버전이있음
-// utf-8 < 현재 인터넷 표준
-// utf-8에도 버전이 있음
+ 숫자, 문자, 논리
+ 숫자 - 실수 정수
+ 문자 - 
+ 문자에 대해 아는만큼
+ 문자와 대입되는 숫자가 있다
+ 아스키코드 128개 정도 묶어둠 -> 부족함
+ 한글 몇개일까요?
+ 유니코드 > 변천사 버전이있음
+ utf-8 < 현재 인터넷 표준
+ utf-8에도 버전이 있음
 
-// 문자는 한글자이다
-// 한글자에 해당하는 자료형? > char (character)
-// 논리형 > 1, 0 한 비트 
-// bool, boolean 불 불린 불리언
+ 문자는 한글자이다
+ 한글자에 해당하는 자료형? > char (character)
+ 논리형 > 1, 0 한 비트 
+ bool, boolean 불 불린 불리언
 
-// 컴퓨터에서 자료를 저장하는 단위
-// 1bit 한 비트 0, 1
-// byte 바이트 > 8 bit
+ 컴퓨터에서 자료를 저장하는 단위
+ 1bit 한 비트 0, 1
+ byte 바이트 > 8 bit
 
-// 실수 정수
-// 정수 0 1 2 3 딱 떨어지는 수
-// short, int, long (2,4,8 바이트)
-// 실수 3.14f
-// float, double
+ 실수 정수
+ 정수 0 1 2 3 딱 떨어지는 수
+ short, int, long (2,4,8 바이트)
+ 실수 3.14f
+ float, double
+ */
+
 
 

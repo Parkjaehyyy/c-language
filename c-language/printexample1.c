e05368f63f75cf3324c15751b3472d7e8b4c7259
Parkjaehyyy <poiuy263@naver.com>
2024-11-19 21:37:50 +0900

    논리연산(true,false)

--- c-language/printexample1.c
+++ c-language/printexample1.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-int main(){
-	printf("hello\n");
-	printf("world\n");
-	printf("\"!@#$%^&*()\\\"");
-	return 0;
-}
+//#include<stdio.h>
+//int main(){
+//	printf("hello\n");
+//	printf("world\n");
+//	printf("\"!@#$%^&*()\\\"");
+//	return 0;
+//}
---
+++ c-language/true.c
@@ -0,0 +1,35 @@
+//#include<stdio.h>
+//#include <stdbool.h>
+//
+//int main() {
+//	bool input1 = true;
+//	bool input2 = true;
+//
+//	int num1 = 15;
+//	// 10대 -> 10~20
+//	// 10 <= 10대 <20
+//	if (10 < num1 && num1 < 20) {
+//		printf("teenager\n");
+//	}
+//
+//	printf("%d \n", true && true);
+//	printf("%d \n", true && false);
+//	printf("%d \n", false && true);
+//	printf("%d \n", false && false);
+//
+//
+//	printf("%d \n", true || true);
+//	printf("%d \n", true || false);
+//	printf("%d \n", false || true);
+//	printf("%d \n", false || false);
+//
+//	printf("%d %d  \n", true, !true);
+//	printf("%d %d  \n", false, !false);
+//
+//
+//	printf("%d \n", (!true && false) || (true && !false));
+//	printf("%d \n", (true || !false) && (false || !true));
+//
+//
+//	return 0;
+//}

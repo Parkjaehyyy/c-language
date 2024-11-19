46fca31d8844b354a405dda603ad953810a77eb2
Parkjaehyyy <poiuy263@naver.com>
2024-11-19 22:12:20 +0900

    scanf ,true

---
+++ c-language/proob.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//int main() {
+//	int number;
+//	int number1;
+//	// &number > number의 주소
+//	// 1~ 100
+//	// 10번지 20번지 30번 ...
+//	scanf_s("%d %d", &number, &number1);
+//	printf("%d", number + number1);
+//	return 0;
+//}
+
+
+int main() {
+	int number;
+
+	scanf_s("%d", &number);
+	
+	// 조건 and 조건 && 
+	if (!(number < 20 || 30 < number)) {
+		printf("teen");
+	}
+	else {
+		printf("not teen");
+	}
+
+	return 0;
+}

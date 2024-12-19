d21f2b36121ce73108d5ed195f51078b37fd9576
Parkjaehyyy <poiuy263@naver.com>
2024-12-19 20:34:44 +0900

    else if 조건문 2

--- c-language/condition.c
+++ c-language/condition.c
@@ -17,3 +17,47 @@ int condtion1(){
 
 	return 0;
 }
+
+
+
+
+int condtion2() {
+	int a = 12;
+	if (a > 10) {
+		printf("10보다 크다");
+	}
+	else if (a > 5) {
+		printf("5보다 크다");
+	}
+	else {
+		printf("5보다 작다");
+	}
+}
+int con3() {
+	int a;
+	printf("점수");
+	scanf_s("%d", &a);
+	if (a >= 90) {
+		printf("A");
+	}
+	else if (a >= 80) {
+		printf("b");
+	}
+	else if (a >= 70) {
+		printf("c");
+	}
+	else if (a >= 60) {
+		printf("d");
+	}
+	else  {
+		printf("F");
+	}
+}
+
+
+
+
+
+
+
+

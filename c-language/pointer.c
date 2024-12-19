cf5fb3dad4253274e3e9605117abc0ca3d522335
Parkjaehyyy <poiuy263@naver.com>
2024-12-19 20:57:24 +0900

    pointer4

---
+++ c-language/pointer.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int pointer1() {
+	int number = 3;
+
+	printf("number의값:%d\n",number);
+	printf("number의주소:%p\n", &number);
+
+}
+int pointer3() {
+	int number = 3;
+	int* pointer = &number;
+	
+	printf("number의값:%d\n", number);
+	printf("number의주소:%p\n", &number);
+	printf("pointer의값:%p\n", pointer);
+	printf("pointer의주소:%d\n", *pointer);
+
+	*pointer = 7;
+	printf("number의값:%d\n", number);
+}
+
+int pointer4() {
+	int number = 3;
+	int* pointer = &number;
+	int** dpointer = &pointer;
+
+	printf("number의값:%d\n", number);
+	printf("number의주소:%p\n", &number);
+	printf("pointer의값:%p\n", pointer);
+	printf("pointer의주소:%d\n", *pointer);
+	printf("dpointer의값:%p\n", dpointer);
+	printf("dpointer의주소:%p\n", *dpointer);
+	printf("dpointer의주소:%d\n", **dpointer);
+	*pointer = 7;
+	printf("number의값:%d\n", number);
+}
+
+

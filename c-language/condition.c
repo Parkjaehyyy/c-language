ac8c775c69997192a026ba76d57dbc219d419d9c
Parkjaehyyy <poiuy263@naver.com>
2024-12-19 20:15:06 +0900

    조건문1

---
+++ c-language/condition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int condtion1(){
+	int a = 7;
+	if (a > 5) {
+		printf("5보다 큽니다.");
+	}
+	else {
+		printf("5보다 작습니다.");
+	}
+
+	if (a > 10) {
+		printf("10보다 큽니다.");
+	}
+	else {
+		printf("10보다 작습니다.");
+	}
+
+	return 0;
+}
